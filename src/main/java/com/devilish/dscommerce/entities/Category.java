10398923c0d7759a0a413c921ec96b848a538a52
Caio Rodrigues <caio.rodrigues@indt.local>
2024-09-23 12:16:50 -0300

    Category Entity

---
+++ src/main/java/com/devilish/dscommerce/entities/Category.java
@@ -0,0 +1,37 @@
+package com.devilish.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category(){
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
