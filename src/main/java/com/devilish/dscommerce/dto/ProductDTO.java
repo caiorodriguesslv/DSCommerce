d3ee461a7ccf1e67ebfcae570cdea0145f0e130e
unknown <caiorodriguesmll@gmail.com>
2024-10-14 18:20:45 -0300

    Product DTO, Services, DTO's, Get product by id

---
+++ src/main/java/com/devilish/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,52 @@
+package com.devilish.dscommerce.dto;
+
+
+import com.devilish.dscommerce.entities.Product;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String imgUrl, Double price, String description, String name) {
+        this.id = id;
+        this.imgUrl = imgUrl;
+        this.price = price;
+        this.description = description;
+        this.name = name;
+    }
+
+    public ProductDTO(Product entity) {
+        this.id = entity.getId();
+        this.imgUrl = entity.getImgUrl();
+        this.price = entity.getPrice();
+        this.description = entity.getDescription();
+        this.name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
